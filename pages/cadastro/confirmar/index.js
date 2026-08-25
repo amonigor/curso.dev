6a0e7061dc740c21ec793fb1289f9cdc7399279b
Igor Amon <48227869+amonigor@users.noreply.github.com>
2026-08-25 11:58:07 -0300

    feat: feat: complete inital layout for `/cadastro` and `/cadastro/confirmar`

---
+++ pages/cadastro/confirmar/index.js
@@ -0,0 +1,19 @@
+import { Banner } from "@primer/react";
+import DefaultLayout from "interface/DefaultLayout";
+
+export default function ConfirmRegisterPage() {
+  return (
+    <DefaultLayout
+      contentWidth="small"
+      metadata={{
+        title: "Confirme seu email",
+      }}
+    >
+      <Banner
+        variant="warning"
+        title="Falta só uma etapa!"
+        description="Abra o email enviado pelo FinTab e clique no link de confirmação."
+      />
+    </DefaultLayout>
+  );
+}
